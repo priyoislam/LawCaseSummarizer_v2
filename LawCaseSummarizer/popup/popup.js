eca9c13a2f8dfa9e02e0792658482be608332ed4
Noor Mohammad <97360324+NoorMohammadPatwary@users.noreply.github.com>
2023-04-25 02:28:02 +0600

    final frontend

--- LawCaseSummarizer/popup/popup.js
+++ LawCaseSummarizer/popup/popup.js
@@ -16,20 +16,72 @@ let openOptionsPage = document.getElementById('openOptionsPage');
 openOptionsPage.addEventListener('click', () => {
   chrome.runtime.sendMessage('showOptions');
 });
-////////////////////////////
 
-const loginSection = document.getElementById("login-section");
-const signupSection = document.getElementById("signup-section");
-const signupButton = document.getElementById("signup-button");
-const loginButton = document.getElementById("login-button");
+//////////////////////////////////////////////
 
-signupButton.addEventListener("click", () => {
-  loginSection.style.display = "none";
-  signupSection.style.display = "block";
+// Get the login and signup forms
+const loginForm = document.getElementById("login-form");
+const signupForm = document.getElementById("signup-form");
+
+// Get the toggle button and back button
+const toggleSignupBtn = document.getElementById("toggle-signup-btn");
+const backToLoginBtn = document.getElementById("back-to-login-btn");
+
+// Handle form submission for login form
+loginForm.addEventListener("submit", (event) => {
+  event.preventDefault(); // Prevent the form from submitting
+
+  // Get the email and password input values
+  const email = document.getElementById("login-email").value;
+  const password = document.getElementById("login-password").value;
+
+  // Do something with the email and password values, such as sending them to a server for authentication
+  console.log(`Logging in with email: ${email} and password: ${password}`);
 });
 
-loginButton.addEventListener("click", () => {
-  signupSection.style.display = "none";
-  loginSection.style.display = "block";
+// Handle form submission for signup form
+signupForm.addEventListener("submit", (event) => {
+  event.preventDefault(); // Prevent the form from submitting
+
+  // Get the email, password, and confirm password input values
+  const email = document.getElementById("signup-email").value;
+  const password = document.getElementById("signup-password").value;
+  const confirmPassword = document.getElementById("confirm-password").value;
+
+  // Do something with the email, password, and confirm password values, such as sending them to a server for creating a new user account
+  console.log(`Signing up with email: ${email} and password: ${password}`);
+
+  // Clear the input fields
+  document.getElementById("signup-email").value = "";
+  document.getElementById("signup-password").value = "";
+  document.getElementById("confirm-password").value = "";
+
+  // Hide the signup form
+  signupForm.style.display = "none";
+  loginForm.style.display = "block";
 });
+
+// Toggle between login and signup forms
+toggleSignupBtn.addEventListener("click", () => {
+  if (loginForm.style.display === "none") {
+    loginForm.style.display = "block";
+    signupForm.style.display = "none";
+    toggleSignupBtn.textContent = "Don't have an account? Signup here."; // Change the button text back to the original text
+  } else {
+    loginForm.style.display = "none";
+    signupForm.style.display = "block";
+    toggleSignupBtn.textContent = "Already have an account? Login here."; // Change the button text to indicate that the user can go back to the login page
+  }
+});
+
+
+// Handle back button click to go back to login form
+backToLoginBtn.addEventListener("click", () => {
+  loginForm.style.display = "block";
+  signupForm.style.display = "none";
+});
+
+
+
+
 
